d828702e09248db106b3993e22aeb0327e08d45c
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2022-10-21 16:13:38 -0300

    Consultar boletim de um aluno

--- Fundamentos do Desenvolvimento JAVA/TP1/ControleAcademico.java
+++ Fundamentos do Desenvolvimento JAVA/TP1/ControleAcademico.java
@@ -9,27 +9,64 @@ public class ControleAcademico {
     private static int vetorIndex = 0;
 
     private static void cadastrarAluno() {  
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o nome do aluno: ");
 
-        String nome = sc.nextLine();
-        System.out.println("Digite a nota da AV1: ");
-        
-        float notaAV1 = sc.nextFloat();
-        System.out.println("Digite a nota da AV2: ");
-        
-        float notaAV2 = sc.nextFloat();
-        
-        alunos[vetorIndex] = nome;
-        notasAV1[vetorIndex] = notaAV1;
-        notasAV2[vetorIndex] = notaAV2;
+        if (vetorIndex < 100) {
+            
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("Digite o nome do aluno: ");
+            
+            String nome = entrada.nextLine();
+            System.out.println("Digite a nota da AV1: ");
+            
+            float notaAV1 = entrada.nextFloat();
+            System.out.println("Digite a nota da AV2: ");
+            
+            float notaAV2 = entrada.nextFloat();
+            
+            if(notaAV1 >= 0 && notaAV1 <= 10 && notaAV2 >= 0 && notaAV2 <= 10) {
+                alunos[vetorIndex] = nome;
+                notasAV1[vetorIndex] = notaAV1;
+                notasAV2[vetorIndex] = notaAV2;
+                vetorIndex++;
+            } else {
+                System.out.println("Nota inválida");
+            }
+            
+            vetorIndex++;
+            
+            System.out.println("Aluno cadastrado com sucesso!");
 
-        vetorIndex++;
-
-        System.out.println("Aluno cadastrado com sucesso!");
+        } else {
+            System.out.println("Limite de alunos atingido!");
+        }
     }
     
-    private static void consultarBoletimAluno() {    }
+    private static void consultarBoletimAluno() {  
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o número do aluno: ");
+
+        int numeroAluno = entrada.nextInt();
+
+        if(numeroAluno >= 0 && numeroAluno < vetorIndex) {
+            float media = (notasAV1[numeroAluno] + notasAV2[numeroAluno]) / 2;
+            String situacao = "Reprovado";
+
+            if(media >= 7) {
+                situacao = "Aprovado";
+            } else if(media >= 4 && media < 7) {
+                situacao = "Prova final";
+            }
+
+            System.out.println("Aluno: " + alunos[numeroAluno]);
+            System.out.println("AV1: " + notasAV1[numeroAluno]);
+            System.out.println("AV2: " + notasAV2[numeroAluno]);
+            System.out.println("Média: " + media);
+            System.out.println("Situação: " + situacao);
+        } else {
+            System.out.println("Aluno não encontrado!");
+        }
+    }
     
     private static void consultarNotasTurma() {    }
     
