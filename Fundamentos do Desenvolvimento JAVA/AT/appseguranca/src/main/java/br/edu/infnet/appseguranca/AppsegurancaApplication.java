3bacb6390f7d49f1b4280f4d618a49d9eea933fb
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2022-12-04 14:24:08 -0300

    Criação pojeto AT

---
+++ Fundamentos do Desenvolvimento JAVA/AT/appseguranca/src/main/java/br/edu/infnet/appseguranca/AppsegurancaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appseguranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsegurancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsegurancaApplication.class, args);
+	}
+
+}
---
+++ Fundamentos do Desenvolvimento JAVA/AT/appseguranca/src/test/java/br/edu/infnet/appseguranca/AppsegurancaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appseguranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsegurancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
