7e4f014ca38b6d16059b46844b07877d8c8a941d
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2022-12-04 15:30:16 -0300

    Atributos Aplicacao

--- Fundamentos do Desenvolvimento JAVA/AT/appseguranca/src/main/java/br/edu/infnet/appseguranca/model/domain/Analise.java
+++ Fundamentos do Desenvolvimento JAVA/AT/appseguranca/src/main/java/br/edu/infnet/appseguranca/model/domain/Analise.java
@@ -7,20 +7,20 @@ public class Analise {
     private String descricao;
     private String cliente;
     private Aplicacao aplicacao;
-    private String demanda;
+    private String atividade;
     private int qtdHoras;
     private String dataInicio;
     private String dataFim;
     private String status;
 
-    public Analise(int id, String titulo, String descricao, String cliente, Aplicacao aplicacao, String demanda,
+    public Analise(int id, String titulo, String descricao, String cliente, Aplicacao aplicacao, String atividade,
             int qtdHoras, String dataInicio, String dataFim, String status) {
         this.id = id;
         this.titulo = titulo;
         this.descricao = descricao;
         this.cliente = cliente;
         this.aplicacao = aplicacao;
-        this.demanda = demanda;
+        this.atividade = atividade;
         this.qtdHoras = qtdHoras;
         this.dataInicio = dataInicio;
         this.dataFim = dataFim;
@@ -67,12 +67,12 @@ public class Analise {
         this.aplicacao = aplicacao;
     }
 
-    public String getDemanda() {
-        return demanda;
+    public String getatividade() {
+        return atividade;
     }
 
-    public void setDemanda(String demanda) {
-        this.demanda = demanda;
+    public void setatividade(String atividade) {
+        this.atividade = atividade;
     }
 
     public int getQtdHoras() {
@@ -115,7 +115,7 @@ public class Analise {
         sb.append(", descricao=").append(descricao);
         sb.append(", cliente=").append(cliente);
         sb.append(", aplicacao=").append(aplicacao);
-        sb.append(", demanda=").append(demanda);
+        sb.append(", atividade=").append(atividade);
         sb.append(", qtdHoras=").append(qtdHoras);
         sb.append(", dataInicio=").append(dataInicio);
         sb.append(", dataFim=").append(dataFim);
--- Fundamentos do Desenvolvimento JAVA/AT/appseguranca/src/main/java/br/edu/infnet/appseguranca/model/domain/Aplicacao.java
+++ Fundamentos do Desenvolvimento JAVA/AT/appseguranca/src/main/java/br/edu/infnet/appseguranca/model/domain/Aplicacao.java
@@ -2,4 +2,89 @@ package br.edu.infnet.appseguranca.model.domain;
 
 public class Aplicacao {
 
+    Analise analise;
+    private int id;
+    private String nome;
+    private String demanda;
+    private String ambiente;
+    private String perimetro;
+    private String endereco;
+    private Vulnerabilidade[] vulnerabilidades;
+
+    public Aplicacao(Analise analise, int id, String nome, String demanda, String ambiente, String perimetro,
+            String endereco, Vulnerabilidade[] vulnerabilidades) {
+        this.analise = analise;
+        this.id = id;
+        this.nome = nome;
+        this.demanda = demanda;
+        this.ambiente = ambiente;
+        this.perimetro = perimetro;
+        this.endereco = endereco;
+        this.vulnerabilidades = vulnerabilidades;
+    }
+
+    public Analise getAnalise() {
+        return analise;
+    }
+
+    public void setAnalise(Analise analise) {
+        this.analise = analise;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDemanda() {
+        return demanda;
+    }
+
+    public void setDemanda(String demanda) {
+        this.demanda = demanda;
+    }
+
+    public String getAmbiente() {
+        return ambiente;
+    }
+
+    public void setAmbiente(String ambiente) {
+        this.ambiente = ambiente;
+    }
+
+    public String getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(String perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Vulnerabilidade[] getVulnerabilidades() {
+        return vulnerabilidades;
+    }
+
+    public void setVulnerabilidades(Vulnerabilidade[] vulnerabilidades) {
+        this.vulnerabilidades = vulnerabilidades;
+    }
+
 }
