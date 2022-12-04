460e75d174a2bf8517724c3932df0aee49bcce29
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2022-12-04 15:43:26 -0300

    Atributos Vulnerabilidade

--- Fundamentos do Desenvolvimento JAVA/AT/appseguranca/src/main/java/br/edu/infnet/appseguranca/model/domain/Vulnerabilidade.java
+++ Fundamentos do Desenvolvimento JAVA/AT/appseguranca/src/main/java/br/edu/infnet/appseguranca/model/domain/Vulnerabilidade.java
@@ -2,4 +2,108 @@ package br.edu.infnet.appseguranca.model.domain;
 
 public class Vulnerabilidade {
 
+    private int id;
+    private String nome;
+    private String descricao;
+    private String recomendacao;
+    private String ataque;
+    private String categoria;
+    private int impacto;
+    private int probabilidade;
+    private String severidade;
+    private String classificacaoOWASP;
+
+    public Vulnerabilidade(int id, String nome, String descricao, String recomendacao, String ataque, String categoria,
+            int impacto, int probabilidade, String severidade, String classificacaoOWASP) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.recomendacao = recomendacao;
+        this.ataque = ataque;
+        this.categoria = categoria;
+        this.impacto = impacto;
+        this.probabilidade = probabilidade;
+        this.severidade = severidade;
+        this.classificacaoOWASP = classificacaoOWASP;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getRecomendacao() {
+        return recomendacao;
+    }
+
+    public void setRecomendacao(String recomendacao) {
+        this.recomendacao = recomendacao;
+    }
+
+    public String getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(String ataque) {
+        this.ataque = ataque;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getImpacto() {
+        return impacto;
+    }
+
+    public void setImpacto(int impacto) {
+        this.impacto = impacto;
+    }
+
+    public int getProbabilidade() {
+        return probabilidade;
+    }
+
+    public void setProbabilidade(int probabilidade) {
+        this.probabilidade = probabilidade;
+    }
+
+    public String getSeveridade() {
+        return severidade;
+    }
+
+    public void setSeveridade(String severidade) {
+        this.severidade = severidade;
+    }
+
+    public String getClassificacaoOWASP() {
+        return classificacaoOWASP;
+    }
+
+    public void setClassificacaoOWASP(String classificacaoOWASP) {
+        this.classificacaoOWASP = classificacaoOWASP;
+    }
 }
